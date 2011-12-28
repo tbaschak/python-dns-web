5636b2da4ff6cd6a85f7f7a92764f57f89d0d5ac
olavgg <olavgg@gmail.com>
2011-12-28 14:02:40 +0100

    added security import fix

--- base/security.py
+++ base/security.py
@@ -1,19 +1,19 @@
 from functools import wraps
 from flask import request
-from base import app
+from base import Base
 
 def withPassword(f):
     @wraps(f)
     def checkPassword(*args, **kwargs):
         # Check HTTP Header
         if request.headers.has_key("Authtoken"):
-            request.headers.get("Authtoken") == app.config["AUTHPASS"]
+            request.headers.get("Authtoken") == Base.app.config["AUTHPASS"]
             return f(*args, **kwargs)
         # Check GET param
         elif (request.args.has_key('password') and
-            request.args.get('password') == app.config["AUTHPASS"]
+            request.args.get('password') == Base.app.config["AUTHPASS"]
             ) or (request.form.has_key('password') and
-            request.form.get('password') == app.config["AUTHPASS"]):
+            request.form.get('password') == Base.app.config["AUTHPASS"]):
             return f(*args, **kwargs)
         else:
             return "no access"
